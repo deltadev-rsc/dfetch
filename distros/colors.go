52a46b0d0771cad02e02f5be189b98255b889934
Deltaqxq <deltaqxq174@mail.ru>
2026-08-19 15:01:49 +0500

    Create colors.go

---
+++ distros/colors.go
@@ -0,0 +1,9 @@
+package distros
+
+const RESET_COLOR = "\x1b[0m"
+const RED_COLOR = "\x1b[31m"
+const GREEN_COLOR = "\x1b[32m"
+const YELLOW_COLOR = "\x1b[33m"
+const BLUE_COLOR = "\x1b[34m"
+const MAGENTA_COLOR = "\x1b[35m"
+const CYAN_COLOR = "\x1b[36m"
